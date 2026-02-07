10ba3160cfb8c3c79f390fa3568919a31524df82
miku acharya <mikuacharya3@gmail.com>
2026-02-08 00:47:48 +0530

    custom logging updated for catch the log

---
+++ logger/custom_logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+class CustomLogger:
+    def __init__(self):
+        #Ensure log directory exist
+        self.log_dir=os.path.join(os.getcwd(),"logs")
+        os.makedirs(self.log_dir,exist_ok=True)
+        #create timestamped log file name
+        log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        log_file_path=os.path.join(self.log_dir,log_file)
+        logging.basicConfig(
+            filename=log_file_path,
+            format="[%(asctime)s] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
+            level=logging.INFO
+        )
+    def get_logger(self,name=__file__):
+        return logging.getLogger(os.path.basename(name))
+if __name__ == "__main__":
+    logger=CustomLogger()
+    logger=logger.get_logger(__file__)
+    logger.info("Custom logger is initialized")
